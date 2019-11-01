3dce8f80a842dc7f2784a73eb409bed841af9add
mobius <sv0220@163.com>
2019-11-01 10:57:57 +0800

    examples: checkpassword finished

--- examples/checkpassword/cpmain.go
+++ examples/checkpassword/cpmain.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	checkPassword "checkpassword/proto"
 	"context"
 	"fmt"
 	"net"
 
-	public "github.com/Ankr-network/dccn-tools/examples/public"
+	checkPassword "github.com/Ankr-network/dccn-tools/examples/checkpassword/proto"
 	"google.golang.org/grpc"
 )
 
@@ -14,7 +13,7 @@ type holder struct{}
 
 func (h *holder) Check(ctx context.Context, req *checkPassword.Request) (*checkPassword.Response, error) {
 	rsp := &checkPassword.Response{
-		Rsp: &public.RspCodeMsg{
+		Rsp: &checkPassword.RspCodeMsg{
 			Code: "0000",
 			Msg:  "do it successfully",
 		},
@@ -23,13 +22,14 @@ func (h *holder) Check(ctx context.Context, req *checkPassword.Request) (*checkP
 }
 
 func main() {
-	l, err := net.Listen("tcp", "localhost:8081")
+	l, err := net.Listen("tcp", "0.0.0.0:8081")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	gs := grpc.NewServer()
 	checkPassword.RegisterCheckPWDServer(gs, &holder{})
+	fmt.Println("start check password service ...")
 	if err := gs.Serve(l); err != nil {
 		fmt.Println("service start failed: ", err)
 		return
--- examples/checkpassword/go.mod
+++ examples/checkpassword/go.mod
@@ -1,11 +1,10 @@
-module checkpassword
+module github.com/Ankr-network/dccn-tools/examples/checkpassword
 
-go 1.13
+go 1.12
 
 require (
-	github.com/Ankr-network/dccn-tools/examples/public v0.0.0-20191031061612-841fefee0292
-	github.com/Ankr-network/dccn-tools/logger v0.0.0-20191030102435-b088744b3ce3
-	github.com/Ankr-network/dccn-tools/zap v0.0.0-20191030102435-b088744b3ce3
+	github.com/Ankr-network/dccn-tools/logger v0.0.0-20191031064411-aa8e15f07578
+	github.com/Ankr-network/dccn-tools/zap v0.0.0-20191031064411-aa8e15f07578
 	github.com/golang/protobuf v1.3.2
 	google.golang.org/grpc v1.24.0
 )
